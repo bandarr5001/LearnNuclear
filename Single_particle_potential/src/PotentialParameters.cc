280436aa30e21fd64410a47e6720decd82d645d1
bandarr5001 <daniel.joseph.barr@gmail.com>
2025-07-29 15:02:47 -0400

    Working single particle potential code, not cleaned up

--- Single_particle_potential/src/PotentialParameters.cc
+++ Single_particle_potential/src/PotentialParameters.cc
@@ -15,6 +15,9 @@ int main() {
 	    << "\n*********************************************************************\n"
 	    << std::endl;
 
+    std::cout << std::fixed;
+    std::cout << std::setprecision(6);
+
     try{
 
       potential_parameters pparams = {
@@ -26,64 +29,16 @@ int main() {
       };
 
       potential_results parameter_results = {
-        .A = -218.699,
-        .B = 166.215,
-        .tau = 2.33376
+        .A = -200.0,
+        .B = 150.0,
+        .tau = 3.0
       };
 
-    double p_momentum_Fermi = std::pow(6.0*std::pow(M_PI,2.0)*pparams.density_/pparams.degeneracy_g_,1.0/3.0); 
-    double energy_epsilon_Fermi = std::sqrt(pparams.mass_*pparams.mass_ + p_momentum_Fermi*p_momentum_Fermi);
+      double tolerance = 1e-8;
 
-    double term1_1 = 2.0*energy_epsilon_Fermi*energy_epsilon_Fermi*energy_epsilon_Fermi*p_momentum_Fermi;
+      parameter_results = get_parameters(parameter_results, tolerance, pparams);
 
-    double term2_1 = energy_epsilon_Fermi*p_momentum_Fermi*pparams.mass_*pparams.mass_;
-
-    double term3_1 = pparams.mass_*pparams.mass_*pparams.mass_*pparams.mass_*std::log((energy_epsilon_Fermi + p_momentum_Fermi)/pparams.mass_);
-
-    double energy_density_FG = (pparams.degeneracy_g_ / (16.0 * M_PI*M_PI))
-    * (term1_1 - term2_1 - term3_1);
-
-    double p_momentum_Fermi_satdense = std::pow(6.0*std::pow(M_PI,2.0)
-    *nSat_in_MeV3/pparams.degeneracy_g_,1.0/3.0);
-
-    double energy_epsilon_Fermi_satdense = std::sqrt(pparams.mass_*pparams.mass_ + 
-        p_momentum_Fermi_satdense*p_momentum_Fermi_satdense);
-
-    double term1_2 = 2.0*energy_epsilon_Fermi_satdense*energy_epsilon_Fermi_satdense*energy_epsilon_Fermi_satdense*p_momentum_Fermi_satdense;
-
-    double term2_2 = energy_epsilon_Fermi_satdense*p_momentum_Fermi_satdense*pparams.mass_*pparams.mass_;
-
-    double term3_2 = pparams.mass_*pparams.mass_*pparams.mass_*pparams.mass_*std::log((energy_epsilon_Fermi_satdense + p_momentum_Fermi_satdense)/pparams.mass_);
-
-    double energy_density_FG_satdense = (pparams.degeneracy_g_ / (16.0 * M_PI*M_PI))
-    * (term1_2 - term2_2 - term3_2);
-
-    double condition1 = (energy_density_FG/nSat_in_MeV3) 
-        + ((parameter_results.A/2.0) + (parameter_results.B/parameter_results.tau))*nSat_in_MeV3 - pparams.mass_ - pparams.binding_energy_;
-
-    double condition2 = energy_epsilon_Fermi_satdense 
-        - (energy_density_FG_satdense/nSat_in_MeV3) + (parameter_results.A/2) + parameter_results.B*((parameter_results.tau - 1)/parameter_results.tau);
-
-      double term1_3 = (pparams.degeneracy_g_*energy_epsilon_Fermi_satdense
-        *p_momentum_Fermi_satdense*p_momentum_Fermi_satdense*p_momentum_Fermi_satdense)/(9*nSat_in_MeV3
-        *M_PI*M_PI);
-
-    double term2_3 = parameter_results.A;//*(par->density_/nSat_in_MeV3);
-
-    double term3_3 = parameter_results.B*(parameter_results.tau-1.0);//*std::pow((par->density_/nSat_in_MeV3),tau - 1.0);
-
-    double K = 9.0 * (term1_3 + term2_3 + term3_3);
-
-    double condition3 = K - pparams.incompress_at_satdense_;
-
-    std::cout << "TEST OF CONDITIONS" << std::endl;
-    std::cout << "condition1 = " << condition1 << std::endl;
-    std::cout << "condition2 = " << condition2 << std::endl;
-    std::cout << "condition3 = " << condition3 << std::endl;
-
-      double tolerance = 1e-6;
-
-      get_parameters(parameter_results, tolerance, pparams);
+      get_single_particle_potential(parameter_results, pparams);
 
       std::cout << "\n\n*********************************************************************"
 	    << "\n* Finished evaluating the potential parameters A, B, and tau for single particle potentials *"
--- Single_particle_potential/src/single_particle_potential.cc
+++ Single_particle_potential/src/single_particle_potential.cc
@@ -86,27 +86,29 @@ int conditions(const gsl_vector* x, void* p,
     double B = gsl_vector_get(x,1);
     double tau = gsl_vector_get(x,2);
     const double energy_density_FG = energy_density_Fermigas(p);
-    const double energy_epsilon_Fermi = fermi_energy(p);
-    const double p_momentum_Fermi = fermi_momentum(p); 
+    //const double energy_epsilon_Fermi = fermi_energy(p);
+    //const double p_momentum_Fermi = fermi_momentum(p); 
     const double energy_density_FG_satdense = energy_density_Fermigas_satdense(p);
     const double energy_epsilon_Fermi_satdense = fermi_energy_satdense(p);
     const double p_momentum_Fermi_satdense = fermi_momentum_satdense(p); 
 
 
     double condition1 = (energy_density_FG/nSat_in_MeV3) 
-        + ((A/2.0) + (B/tau))*nSat_in_MeV3 - par->mass_ - par->binding_energy_;
+        + ((A/2.0) + (B/tau))/*nSat_in_MeV3*/ - par->mass_ - par->binding_energy_;
 
     gsl_vector_set(f, 0, condition1);
 
 
-    double condition2 = energy_epsilon_Fermi_satdense 
-        - (energy_density_FG_satdense/nSat_in_MeV3) + (A/2) + B*((tau - 1)/tau);
+    double condition2 = (energy_epsilon_Fermi_satdense/nSat_in_MeV3)
+        - (energy_density_FG_satdense/(nSat_in_MeV3*nSat_in_MeV3)) + ((A/2.0)*(1/nSat_in_MeV3)) + B*((tau - 1.0)/tau)*(1/nSat_in_MeV3);
 
     gsl_vector_set(f, 1, condition2);
 
-    double term1 = (par->degeneracy_g_*energy_epsilon_Fermi_satdense
-        *p_momentum_Fermi_satdense*p_momentum_Fermi_satdense*p_momentum_Fermi_satdense)/(9*nSat_in_MeV3
-        *M_PI*M_PI);
+    //Calculated using Mathematica to brute force the deriivative of P_FG
+    double term1 = (par->degeneracy_g_*p_momentum_Fermi_satdense*
+        p_momentum_Fermi_satdense*p_momentum_Fermi_satdense*
+        p_momentum_Fermi_satdense*p_momentum_Fermi_satdense)
+        /(18.0*energy_epsilon_Fermi_satdense*M_PI*M_PI*nSat_in_MeV3);
 
     double term2 = A;//*(par->density_/nSat_in_MeV3);
 
@@ -118,7 +120,7 @@ int conditions(const gsl_vector* x, void* p,
 
     gsl_vector_set(f, 2, condition3);
 
-    std::cout << std::setprecision(10);
+    /*std::cout << std::setprecision(10);
     std::cout << "[DEBUG] Inputs: A = " << A << ", B = " << B << ", tau = " << tau << std::endl;
     std::cout << "[DEBUG] Nuclear Matter Properties: g = " << par->degeneracy_g_ << ", mass = " << par->mass_ << ", density = " << par->density_ << ", B_0 = " << par->binding_energy_ << ", K_0 = " << par->incompress_at_satdense_ << std::endl;
     std::cout << "[DEBUG] fermi_momentum = " << p_momentum_Fermi << std::endl;
@@ -126,7 +128,7 @@ int conditions(const gsl_vector* x, void* p,
     std::cout << "[DEBUG] energy_density_FG = " << energy_density_FG << std::endl;
     std::cout << "[DEBUG] condition1 = " << condition1 << std::endl;
     std::cout << "[DEBUG] condition2 = " << condition2 << std::endl;
-    std::cout << "[DEBUG] condition3 = " << condition3 << std::endl;
+    std::cout << "[DEBUG] condition3 = " << condition3 << std::endl;*/
 
     if (!std::isfinite(condition1) || !std::isfinite(condition2) || !std::isfinite(condition3)) {
         std::cerr << "NaN or Inf in conditions!" << std::endl;
@@ -157,7 +159,7 @@ potential_results get_parameters(potential_results parameter_results, double tol
 
   int test_status;
 
-  size_t i = 0, iMAX = 100;
+  size_t i = 0, iMAX = 1000;
 
   do {
     i++;
@@ -168,7 +170,7 @@ potential_results get_parameters(potential_results parameter_results, double tol
     }
     test_status = gsl_multiroot_test_residual(s->f, tolerance);
     
-    std::cout << "i: " << i << std::endl;
+    //std::cout << "i: " << i << std::endl;
   } while (test_status == GSL_CONTINUE && i < iMAX);
 
   parameter_results.A = gsl_vector_get(s->x,0);
@@ -177,7 +179,7 @@ potential_results get_parameters(potential_results parameter_results, double tol
     
   if (test_status == GSL_SUCCESS) {
     std::cout << "Single particle potential parameters for V(" << pparams.density_ <<") at T = 0: A = " 
-	      << parameter_results.A << " B = " << parameter_results.B << " tau = " << parameter_results.tau << " MeV" << std::endl;
+	     << parameter_results.A << " B = " << parameter_results.B << " tau = " << parameter_results.tau << " MeV" << std::endl;
   }
 
   else {
@@ -190,7 +192,13 @@ potential_results get_parameters(potential_results parameter_results, double tol
 
 double get_single_particle_potential(potential_results parameter_results,
      potential_parameters pparams) {
-    double potential = parameter_results.A*(pparams.density_/nSat_in_MeV3) + parameter_results.B*std::pow(pparams.density_/nSat_in_MeV3,parameter_results.tau - 1.0);
-    std::cout << "Single particle potential parameters for V(" << pparams.density_ << ") = " << potential << " MeV" << std::endl;
+
+    double potential = parameter_results.A*(pparams.density_/nSat_in_MeV3) +
+     parameter_results.B*
+     std::pow(pparams.density_/nSat_in_MeV3,parameter_results.tau - 1.0);
+
+    std::cout << "Single particle potential for V(n_B = " << pparams.density_ 
+    << ") = " << potential << " MeV" << std::endl;
+    
     return potential; 
 }
